b08634aa569abf88500488a3c1e6efc78652f1c9
Carl-Rabbit <479843162@qq.com>
2023-09-15 20:19:29 +0800

    save

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, request, render_template, send_file, redirect, url_for
+
+
+app = Flask(__name__, template_folder="./dist", static_folder="./dist/static")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data/<path:path>')
+def send_data(path):
+    return send_file(os.path.join('./dist/data', path))
+
+if __name__ == '__main__':
+    print('server start')
+    app.run(host='0.0.0.0', port=7999, debug=False)
